dcc47c838f2ddd0edb2c918805419bb4c7931a52
sanjib <sanjib@technometrics.net>
2024-04-08 20:49:32 +0000

    eslint is working

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,8 @@ import eslintPluginAstro from 'eslint-plugin-astro';
 export default [
     // add more generic rule sets here, such as:
     // js.configs.recommended,
-    ...eslintPluginAstro.configs['flat/recommended'],
+    ...eslintPluginAstro.configs['flat/all'],
+    ...eslintPluginAstro.configs['flat/jsx-a11y-strict'],
     {
         rules: {
             // override/add rules settings here, such as:
