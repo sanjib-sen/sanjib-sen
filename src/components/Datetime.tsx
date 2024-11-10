a545390c0be36698bd5e7b568d8cc3ef41e72aea
Sanjib Kumar Sen <sksenonline@gmail.com>
2024-11-11 04:00:54 +0600

    Added prettier formatter

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -30,8 +30,9 @@ export default function Datetime({
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className={`${size === "sm" ? "scale-90" : "scale-100"
-          } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
+        className={`${
+          size === "sm" ? "scale-90" : "scale-100"
+        } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
         aria-hidden="true"
       >
         <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
