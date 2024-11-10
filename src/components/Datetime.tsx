48ea9791f00a9ada19595341c468fd99188e34c8
Sanjib Kumar Sen <sksenonline@gmail.com>
2024-11-11 05:59:29 +0600

    pretty suggest changes

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -26,32 +26,35 @@ export default function Datetime({
 }: Props) {
   return (
     <div
-      className={`flex items-center space-x-2 opacity-80 ${className}`.trim()}
+      className={`flex flex-wrap justify-between gap-1.5 items-center opacity-80 ${className}`.trim()}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={`${
-          size === "sm" ? "scale-90" : "scale-100"
-        } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
-        aria-hidden="true"
-      >
-        <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
-        <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
-      </svg>
-      {modDatetime && modDatetime > pubDatetime ? (
+      <div className="flex items-center gap-1.5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={`${size === "sm" ? "scale-90" : "scale-100"
+            } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
+          aria-hidden="true"
+        >
+          <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
+          <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
+        </svg>
+        {modDatetime && modDatetime > pubDatetime ? (
+          <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+            Updated:
+          </span>
+        ) : (
+          <span className="sr-only">Published:</span>
+        )}
         <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
+          <FormattedDatetime
+            pubDatetime={pubDatetime}
+            modDatetime={modDatetime}
+          />
         </span>
-      ) : (
-        <span className="sr-only">Published:</span>
-      )}
-      <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-        <FormattedDatetime
-          pubDatetime={pubDatetime}
-          modDatetime={modDatetime}
-        />
-        {size === "lg" && <EditPost editPost={editPost} postId={postId} />}
-      </span>
+      </div>
+      <div>
+        <EditPost editPost={editPost} postId={postId} />
+      </div>
     </div>
   );
 }
@@ -95,7 +98,7 @@ const EditPost = ({ editPost, postId }: EditPostProps) => {
   return (
     showEditPost && (
       <>
-        <span aria-hidden="true"> | </span>
+        {/* <div aria-hidden="true" className="hidden sm:block"> | </div> */}
         <a
           className="space-x-1.5 hover:opacity-75"
           href={editPostUrl}
